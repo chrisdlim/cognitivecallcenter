58173d8ec3d6399ac3b2230c0ca440f8a42f5c3e
Kevin Raoofi <kevin.raoofi@ibm.com>
2017-10-20 15:13:33 -0400

    added the cloudant service boilerplate

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-
-router.get('/', function(req, res){
+router.get('/', function(req, res) {
   res.json({ title: 'Cloudant Boiler Plate' });
 });;
 
