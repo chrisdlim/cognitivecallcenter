568024c4353350736a1669dedc784f14862ab1bb
Kevin Raoofi <kevin.raoofi@ibm.com>
2017-10-20 23:19:36 -0400

    eslint is good

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 router.get('/foobar', function(req, res) {
   res.json({ title: 'Cloudant Boiler Plate' });
-});;
+});
 
 module.exports = () => router;
--- routes/twilio_listener.js
+++ routes/twilio_listener.js
@@ -10,69 +10,69 @@ function initialize(sio) {
   router.use(urlencoded({extended: false}));
 
   router.post('/record', function(req, res) {
-      const twiml = new VoiceResponse();
-      twiml.say('hello. please leave a message after the beep.\press the star key when finished.');
+    const twiml = new VoiceResponse();
+    twiml.say('hello. please leave a message after the beep. press the star key when finished.');
 
     twiml.record({
-    	timeout: 5,
-        transcribe: false,
-        recordingStatusCallback: '/handleRecording',
-        maxLength: 20,
-        action: '/handleRecording'
+      timeout: 5,
+      transcribe: false,
+      recordingStatusCallback: '/handleRecording',
+      maxLength: 20,
+      action: '/handleRecording'
     });
 
-      twiml.hangup();
+    twiml.hangup();
 
-      res.type('text/xml');
-      console.log(twiml.toString());
-      console.log(util.inspect(req));
-      res.send(twiml.toString());
+    res.type('text/xml');
+    console.log(twiml.toString());
+    console.log(util.inspect(req));
+    res.send(twiml.toString());
   });
-  
+
   router.post('/handleRecording', function(req, res) {
-      var body = req.body;
-     console.log(util.inspect(body));
+    var body = req.body;
+    console.log(util.inspect(body));
   });
 
   // Create a route that will handle Twilio webhook requests, sent as an
   // HTTP POST to /voice in our application
   router.post('/voice', function(req, res) {
-      // Get information about the incoming call, like the city associated
-      // with the phone number (if Twilio can discover it)
-      const city = req.body.FromCity;
+    // Get information about the incoming call, like the city associated
+    // with the phone number (if Twilio can discover it)
+    const city = req.body.FromCity;
 
-      // Use the Twilio Node.js SDK to build an XML response
-      const twiml = new VoiceResponse();
-      twiml.say({voice: 'alice'},
-        `Never gonna give you up ${city}.`
-      );
-      twiml.play({}, 'https://demo.twilio.com/docs/classic.mp3');
+    // Use the Twilio Node.js SDK to build an XML response
+    const twiml = new VoiceResponse();
+    twiml.say({voice: 'alice'},
+      `Never gonna give you up ${city}.`
+    );
+    twiml.play({}, 'https://demo.twilio.com/docs/classic.mp3');
 
-      // Render the response as XML in reply to the webhook request
-      res.type('text/xml');
-      res.send(twiml.toString());
-    });
+    // Render the response as XML in reply to the webhook request
+    res.type('text/xml');
+    res.send(twiml.toString());
+  });
 
   // Create a route that will handle Twilio webhook requests, sent as an
   // HTTP POST to /voice in our application
   router.post('/voice', function(req, res) {
-      // Get information about the incoming call, like the city associated
-      // with the phone number (if Twilio can discover it)
-      const city = req.body.FromCity;
+    // Get information about the incoming call, like the city associated
+    // with the phone number (if Twilio can discover it)
+    const city = req.body.FromCity;
 
-      // Use the Twilio Node.js SDK to build an XML response
-      const twiml = new VoiceResponse();
-      twiml.say({voice: 'alice'},
-        `Never gonna give you up ${city}.`
-      );
-      twiml.play({}, 'https://demo.twilio.com/docs/classic.mp3');
+    // Use the Twilio Node.js SDK to build an XML response
+    const twiml = new VoiceResponse();
+    twiml.say({voice: 'alice'},
+      `Never gonna give you up ${city}.`
+    );
+    twiml.play({}, 'https://demo.twilio.com/docs/classic.mp3');
 
-      // Render the response as XML in reply to the webhook request
-      res.type('text/xml');
-      res.send(twiml.toString());
-    });
+    // Render the response as XML in reply to the webhook request
+    res.type('text/xml');
+    res.send(twiml.toString());
+  });
 
-    return router;
+  return router;
 }
 
 module.exports = initialize;
