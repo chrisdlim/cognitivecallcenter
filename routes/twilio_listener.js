060da6a8652ecbb2814f83fe426a4f8ad217fa9c
Kevin Raoofi <kevin.raoofi@ibm.com>
2017-10-21 01:33:54 -0400

    made https

--- routes/twilio_listener.js
+++ routes/twilio_listener.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const http = require('http');
+const http = require('https');
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const urlencoded = require('body-parser').urlencoded;
 const util = require('util');
