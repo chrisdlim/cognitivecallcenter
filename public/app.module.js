3d763286cd8bc527f2ce26567a94c2c028886e59
Chris Lim <chris.lim1@ibm.com>
2017-10-21 05:19:00 -0400

    5am

--- public/app.module.js
+++ public/app.module.js
@@ -19,31 +19,38 @@ app.config(function($routeProvider) {
     })
      .when("/details", {
         templateUrl : "/view/details.html",
-        controller: "detailsCtrl",
-        paramExample: 'exampleB'
+        controller: "detailsCtrl"
     });
 });
 
-app.controller('homeCtrl', ['$scope', 'SocketService', function($scope, SocketService) {
+app.controller('homeCtrl', ['$scope', 'SocketService', '$http' function($scope, SocketService, $http) {
     var self = this;
     self.ss = SocketService;
 
     self.ss.register($scope, (data) => {
-      console.log(data);
+      // console.log(data);
     });
+    var url = "";
+
+    $http.get(url)
+      .then(function(response){
+        //process list of data
+      });
 
     self.callers = [
         {
             id: "203-907-7424",
             name: "Chris Lim",
-            action: "called",
-            mood: 12
+            action: "in-progress",
+            mood: 12,
+            transcript: "I love monica I love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monicaI love monica"
         },
         {
             id: "973-203-4657",
             name: "Morgue Town",
-            action: "hung up",
-            mood: 96
+            action: "hung-up",
+            mood: 96,
+            transcript: "Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! Kate doesn't like me! "
         },
         {
             id: "561-543-2344",
@@ -54,7 +61,7 @@ app.controller('homeCtrl', ['$scope', 'SocketService', function($scope, SocketSe
         {
             id: "561-543-2344",
             name: "Kevin's mom",
-            action: "update",
+            action: "in-progress",
             mood: 78
         },
         {
@@ -66,13 +73,20 @@ app.controller('homeCtrl', ['$scope', 'SocketService', function($scope, SocketSe
         {
             id: "561-543-2344",
             name: "Monica the love of my life",
-            action: "update",
+            action: "hung-up",
             mood: 44
         }
     ];
+
 }]);
 
-app.controller('detailsCtrl', ['$scope', '$route', function ($scope, $route) {
-    var paramValue = $route.current.$$route.paramExample;
-    console.log(paramValue);
+app.controller('detailsCtrl', ['$scope', '$route', '$routeParams', '$location', function ($scope, $route, $routeParams, $location) {
+  var caller   = $location.search();
+  var self = this;
+
+  self.caller = caller;
+
+  $scope.connectCall = function(){
+    console.log("Connecting with: " + caller.id);
+  }
 }]);
